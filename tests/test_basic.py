cba35593a66bd458573a5dffe2022abfb1f890ff
Ankush Chatterjee <ac.ankush15@gmail.com>
2026-02-03 16:53:03 +0530

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -25,7 +25,7 @@ require_cpython_gc = pytest.mark.skipif(
     python_implementation() != "CPython",
     reason="Requires CPython GC behavior",
 )
-#t5
+#t9
 
 def test_options_work(app, client):
     @app.route("/", methods=["GET", "POST"])
